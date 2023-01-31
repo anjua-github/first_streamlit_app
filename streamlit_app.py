e1272868bd60085cd9202a1c549d0265e9bc2063
anjua-github <122079789+anjua-github@users.noreply.github.com>
2023-01-31 15:15:46 +0530

    Pandas for dataframe (my_fruit_list)
    imported pandas and read_csv into my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,3 +10,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+steamlit.dataframe(my_fruit_list)
