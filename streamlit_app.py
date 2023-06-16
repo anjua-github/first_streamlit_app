3cdd92dae4734bf60726563d0230a2b941dd3a05
anjua-github <122079789+anjua-github@users.noreply.github.com>
2023-06-16 15:52:15 +0530

    Update streamlit_app.py
    added emojis and one extra item to breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 &Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Eggs')
+streamlit.text('🥣 Omega 3 &Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
+streamlit.text('🥑🍞 Avocado Toast')
